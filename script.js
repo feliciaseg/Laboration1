ba0c643d04b76939c49d66a7e36ee6129793fd10
Felicia Segerholm <feliciasegerholm@Felicias-MacBook-Air-3.local>
2020-10-28 14:52:46 +0100

    First promps & alerts

---
+++ script.js
@@ -0,0 +1,9 @@
+function startGame () {
+
+    alert ("Nu kör vi!")
+
+   let name = prompt("Vad heter du?");
+
+    alert("Välkommen " + name + ". Du befinner dig just nu på caféet Colorado. Du ")
+
+}
